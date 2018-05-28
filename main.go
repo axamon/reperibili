5209635dac9538fe1616b90a74e4ed05abcbbf80
Alberto Bregliano <alberto.bregliano@pm.me>
2018-05-28 14:22:17 +0200

    pulizia e nuove func

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/axamon/cripta"
 )
 
 //Reperibile è la variabile con i dati personali dei reperibili
@@ -110,19 +112,6 @@ func Reperibiliperpiattaforma2(piatta, file string) (contatto Reperibile, err er
 	return contatto, fmt.Errorf("%s", "Nessun reperibile trovato")
 }
 
-/* func main() {
-
-	flag.Parse()
-
-	rep, err := Reperibiliperpiattaforma(*piattaforma)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(rep.Cellulare)
-
-} */
-
 //Verificacellulare risponde ok se il numero inzia con +3 e si compone di 10 cifre
 func Verificacellulare(CELLULARE string) (ok bool) {
 
@@ -166,83 +155,6 @@ func Inseriscireperibile(GIORNO, PIATTAFORMA, GRUPPO, NOME, COGNOME, CELLULARE s
 	return true
 }
 
-//Reperibiliperpiattaforma restituisce il reperibile attuale per la piattaforma passata come parametro
-func Reperibiliperpiattaforma(piattaforma string) (contatto Reperibile, err error) {
-	flag.Parse()
-
-	Caricareperibili()
-	Mostrareperibili()
-
-	for _, reperibile := range Mostrareperibili() {
-		if reperibile.Assegnazione.Piattaforma == piattaforma {
-			contatto := reperibile
-			return contatto, nil
-		}
-	}
-
-	return contatto, fmt.Errorf("%s", "Nessun reperibile trovato")
-}
-
-//Mostrareperibili mostra i reperibili
-func Mostrareperibili() (reperibili []Reperibile) {
-	Caricareperibili()
-	//TODO per ogni piattaforma mostra il reperibile attuale
-	for _, contatto := range contatti {
-
-		if contatto.Assegnazione.Piattaforma != "CDN" {
-			if t.Before(limite7) {
-				if contatto.Assegnazione.Giorno == ieri {
-
-					fmt.Println(contatto.Assegnazione.Piattaforma, contatto.Cognome, contatto.Cellulare)
-
-				}
-			} else if contatto.Assegnazione.Giorno == oggi {
-
-				reperibili = append(reperibili, contatto)
-				//fmt.Println(contatto.Assegnazione.Piattaforma, contatto.Cognome, contatto.Cellulare)
-
-			}
-
-		}
-		if contatto.Assegnazione.Giorno == oggi && contatto.Assegnazione.Piattaforma == "CDN" {
-
-			reperibili = append(reperibili, contatto)
-			//fmt.Println(contatto.Assegnazione.Piattaforma, contatto.Cognome, contatto.Cellulare)
-
-		}
-	}
-	return reperibili
-}
-
-//Caricareperibili carica i reperibili in contatti
-func Caricareperibili() (contatti []Reperibile) {
-	csvFile, _ := os.Open(*filecsv)
-	defer csvFile.Close()
-	reader := csv.NewReader(bufio.NewReader(csvFile))
-	//var contatti []Reperibile
-	for {
-		line, error := reader.Read()
-		if error == io.EOF {
-			break
-		} else if error != nil {
-			log.Fatal(error)
-		}
-		contatti = append(contatti, Reperibile{
-
-			Nome:      line[3],
-			Cognome:   line[4],
-			Cellulare: line[5],
-			Assegnazione: Assegnazione{
-				Giorno:      line[0],
-				Piattaforma: line[1],
-				Gruppo:      line[2],
-			},
-		})
-	}
-	//fmt.Println(contatti)
-	return
-}
-
 func recuperavariabile(variabile string) (result string, err error) {
 	if result, ok := os.LookupEnv(variabile); ok && len(result) != 0 {
 		return result, nil
@@ -250,94 +162,12 @@ func recuperavariabile(variabile string) (result string, err error) {
 	return "", fmt.Errorf("la variabile %s non esiste o è vuota", variabile)
 }
 
-/* func main() {
-	TO := os.Args[1]
-	NOME := os.Args[2]
-
-	sid, err := Chiamareperibile(TO, NOME)
-	if err != nil {
-		fmt.Println(err)
+func recuperavariabilecifrata(variabile, passwd string) (result string, err error) {
+	if str, ok := os.LookupEnv(variabile); ok && len(str) != 0 {
+		result = cripta.Decifra(str, passwd)
+		return result, nil
 	}
-
-	fmt.Println(sid)
-
-} */
-
-func call(TO string) (sid string) {
-
-	twilionumber, err := recuperavariabile("TWILIONUMBER")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(100)
-	}
-	// Let's set some initial default variables
-
-	//Recupera l'accountsid di Twilio dallla variabile d'ambiente
-	accountSid, err := recuperavariabile("TWILIOACCOUNTSID")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(101)
-	}
-
-	//Recupera il token supersegreto dalla variabile d'ambiente
-	authToken, err := recuperavariabile("TWILIOAUTHTOKEN")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(102)
-	}
-
-	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Calls.json"
-	// Build out the data for our message
-	v := url.Values{}
-	//v.Set("status_callback", "http://sauron1.westeurope.cloudapp.azure.com:3000/status")
-	//v.Set("status_callback_event", "initiated")
-	v.Set("To", TO)
-	NOME := "Gringo"
-	COGNOME := "Gallo"
-	//v.Set("NOME", "Gringo")	v.S	v.Set("From", twilionumber)
-	v.Set("From", twilionumber)
-
-	//v.Set("COGNOME", "Gaggio")
-	v.Set("From", twilionumber)
-	//Sfortunatamente la URL deve essere Pubblica se no twilio non può arrivarci
-	v.Set("Url", "https://handler.twilio.com/twiml/EH5cef42aa1454fc2326780c8f08c6d568?NOME="+NOME+"&COGNOME="+COGNOME)
-	rb := *strings.NewReader(v.Encode())
-
-	// Create Client
-	client := &http.Client{}
-
-	//Prepara la richiesta HTTP
-	req, err := http.NewRequest("POST", urlStr, &rb)
-	if err != nil {
-		fmt.Fprintln(os.Stdout, "OH noooo! Qualcosa è andata storta nel creare la richiesta", err)
-	}
-
-	//Utiliziamo l'autenticazione basic
-	req.SetBasicAuth(accountSid, authToken)
-	//Inseriamo un po' di headers come piacciono a Twilio
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	// make request
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Fprintln(os.Stdout, "OH noooo! Qualcosa è andata storta nell'inviare la richiesta", err.Error())
-	}
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		var data map[string]interface{}
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		err := json.Unmarshal(bodyBytes, &data)
-		if err == nil {
-
-			//fmt.Println(data) //debug
-			//	fmt.Println(data["sid"]) //debug
-			sid := data["sid"].(string)
-			return sid
-
-		}
-
-	}
-	return
+	return "", fmt.Errorf("la variabile %s non esiste o è vuota", variabile)
 }
 
 //Chiamareperibile e comunica il problema
